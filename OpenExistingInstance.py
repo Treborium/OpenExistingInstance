ad85028b73b1d75976446de5803835eb1d6c4607
Robert Fuchs <robert.fuchs@logmein.com>
2018-06-19 13:51:38 +0200

    implement logic to handle open instances

--- OpenExistingInstance.py
+++ OpenExistingInstance.py
@@ -1,1 +1,27 @@
 #! /usr/bin/python3
+import subprocess
+
+
+def is_instance_already_open(application):
+    output = subprocess.run(
+        # list all running windows
+        ["wmctrl", "-l"], stdout=subprocess.PIPE).stdout.decode("utf-8")
+    return application in output
+
+
+def open_new_instance_of(application):
+    # Open a new instance of given application
+    # Focus new instance
+    subprocess.run([application])
+
+
+def focus_instance_of(application):
+    # Moves the window to the current desktop, raises it und gives it focus
+    subprocess.call(application)
+
+
+application = "Hyper"
+if (is_instance_already_open(application)):
+    focus_instance_of(application)
+else:
+    open_new_instance_of(application)
