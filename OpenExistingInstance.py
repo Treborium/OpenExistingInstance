7709d90f7e76e0d3a790ccafe318a9cd5ccf25ed
Robert Fuchs <robert.fuchs@logmein.com>
2018-06-19 16:28:02 +0200

    only use 2nd argument if provided otherwise use first for new app

--- OpenExistingInstance.py
+++ OpenExistingInstance.py
@@ -32,11 +32,14 @@ def focus_instance_of(application):
 
 def run():
     logging.basicConfig(filename="OpenExistingInstance.debug", level=logging.DEBUG)
+
     application = sys.argv[1]
+    new_application = sys.argv[2] if len(sys.argv) > 1 else application
+    
     if (is_instance_already_open(application)):
         focus_instance_of(application)
     else:
-        open_new_instance_of(sys.argv[2])
+        open_new_instance_of(new_application)
 
 
 if __name__ == "__main__":
