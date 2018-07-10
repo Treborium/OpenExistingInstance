ae29f3f727109842528fdfa3fd80bb4628238a20
Robert Fuchs <RobertFuchs97@gmail.com>
2018-07-10 22:31:58 +0200

    implement methods to retrieve id of instance and to check wether its already open

--- FocusInstance.py
+++ FocusInstance.py
@@ -6,8 +6,28 @@ import subprocess
 import sys
 
 
+def extract_output_from_completed_process(process):
+    return process.stdout.decode('utf-8')
+
+
+def get_id_of(app):
+    output = extract_output_from_completed_process(
+        subprocess.run(["xdotool", "search", app], stdout=subprocess.PIPE))
+
+    if not output:
+        return None
+    # return second last element because the last one is an empty string
+    return output.split('\n')[-2]
+
+
 def is_instance_already_open(app):
-    return subprocess.run(["xdotool", "search", "--sync", app], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    return get_id_of(app) != None
+
+
+def move_and_raise_instance_of(app):
+    desktop_number = extract_output_from_completed_process(
+        subprocess.run(["xdotool", "get_desktop"], stdout=subprocess.PIPE))
+    subprocess.run(["xdotool", "set_desktop_for_window", app, desktop_number])
 
 
 def setup_logging():
@@ -20,4 +40,5 @@ def setup_logging():
 
 if __name__ == "__main__":
     # setup_logging()
+
     print(is_instance_already_open("gedit"))
