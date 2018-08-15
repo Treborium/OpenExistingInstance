f7290ad8ba152951ed3ebb77d9dd7956a2cde921
Robert Fuchs <robertfuchs97@gmail.com>
2018-08-15 10:11:13 +0200

    Add type hinting to functions

--- wmctrl/OpenExistingInstance.py
+++ wmctrl/OpenExistingInstance.py
@@ -5,20 +5,20 @@ import sys
 import os
 
 
-def get_running_instances():
+def get_running_instances() -> str:
     return subprocess.run(
         # list all running windows
         ["wmctrl", "-l"], stdout=subprocess.PIPE).stdout.decode("utf-8").lower()
 
 
-def get_id_from_latest_instance_of(application):
+def get_id_from_latest_instance_of(application: str) -> int:
     running_applications = get_running_instances().split("\n")
     for line in reversed(running_applications):
         if application in line:
             return line.split(" ")[0]
 
 
-def is_instance_already_open(application):
+def is_instance_already_open(application: str) -> bool:
     running_applications = get_running_instances()
 
     logging.debug("is_instance_already_open:\n{0}".format(
@@ -26,7 +26,7 @@ def is_instance_already_open(application):
     return application in running_applications
 
 
-def open_new_instance_of(application):
+def open_new_instance_of(application: str) -> None:
     return_code = subprocess.call([application])
     if return_code != 0:
         # TODO: handle error
@@ -37,18 +37,18 @@ def open_new_instance_of(application):
             "open_new_instance_of: Opening new instance of {0}".format(application))
 
 
-def focus_instance_of(application):
+def focus_instance_of(application: str) -> None:
     app_id = get_id_from_latest_instance_of(application)
     if app_id == None:
         # TODO: return meaningfull error message
         raise AttributeError("No app found")
-    # Moves the window to the current desktop, raises it und gives it focus
+    # Move the window to the current desktop, raise it und give it focus
     subprocess.run(["wmctrl", "-i", "-R", app_id])
     logging.debug(
         "focus_instance_of: Putting {0} with ID = {1} in focus".format(application, app_id))
 
 
-def setup_logging():
+def setup_logging() -> None:
     logFormatter = '%(asctime)s - %(levelname)s - %(message)s'
     file_path = os.path.realpath("OpenExistingInstance.log")
     logging.basicConfig(
